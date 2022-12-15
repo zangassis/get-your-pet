60c62c29b6b45bf81517caec850d1d788681e97b
Assis Zang <assis.compasso@dotzmkt.com.br>
2022-12-15 13:16:07 -0300

    feat: Add login user functions

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,9 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 module.exports = class UserController {
     static async register(req, res) {
@@ -83,5 +86,104 @@ module.exports = class UserController {
         }
 
         await createUserToken(user, req, res)
+    }
+
+    static async checkUser(req, res) {
+        let currentUser;
+
+        if (req.headers.authorization) {
+            const token = getToken(req);
+            const decoded = jwt.verify(token, "secret");
+
+            currentUser = await User.findById(decoded.id);
+            currentUser.password = undefined;
+        } else {
+            currentUser = null;
+        }
+
+        res.status(200).send(currentUser);
+    }
+
+    static async getUserById(req, res) {
+        const id = req.params.id
+
+        const user = await User.findById(id).select('-password')
+
+        if (!user) {
+            res.status(404).json({ message: 'User not found' })
+            return
+        }
+        res.status(200).json({ user })
+    }
+
+    static async editUser(req, res) {
+
+        const id = req.params.id
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const { name, email, phone, password, confirmpassword } = req.body
+
+        let image = ''
+
+        if (!name) {
+            res.status(422).json({ message: 'Name is required' })
+            return
+        }
+        if (!email) {
+            res.status(422).json({ message: 'Email is required' })
+            return
+        }
+
+        const userExists = await User.findOne({ email: email })
+
+        if (user.email !== email && userExists) {
+            res.status(422).json({ message: 'E-mail already registered' })
+            return
+        }
+        if (!phone) {
+            res.status(422).json({ message: 'Phone is required' })
+            return
+        }
+
+        user.phone = phone
+
+        if (!password) {
+            res.status(422).json({ message: 'Password is required' })
+            return
+        }
+        if (password !== confirmpassword) {
+            res.status(422).json({ message: 'Passwords must be the same' })
+            return
+        } else if (password === confirmpassword && password != null) {
+
+            const salt = await bcrypt.genSalt(12)
+            const passwordHash = await bcrypt.hash(password, salt)
+
+            user.password = passwordHash
+        }
+
+        try {
+            await User.findOneAndUpdate({ _id: user._id }, { $set: user }, { new: true })
+            res.status(200).json({ message: 'User updated successfully' })
+        } catch (error) {
+            res.status(500).json({ message: error })
+            return
+        }
+
+        console.log(user)
+
+        if (!confirmpassword) {
+            res.status(422).json({ message: 'Password Confirm is required' })
+            return
+        }
+        if (password !== confirmpassword) {
+            res.status(422).json({ message: 'Password and confirmation must be the same' })
+            return
+        }
+        if (!user) {
+            res.status(422).json({ message: 'User not found' })
+            return
+        }
     }
 }
---
+++ backend/helpers/get-user-by-token.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+const getUserByToken = async(token) => {
+
+    if (!token) {
+        return res.status(401).json({ message: 'Access danied' })
+    }
+
+    const decoded = jwt.verify(token, 'secret')
+    const userId = decoded.id
+    const user = await User.findOne({ _id: userId })
+    return user
+}
+
+module.exports = getUserByToken
